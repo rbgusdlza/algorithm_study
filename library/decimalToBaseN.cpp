3a1654367e556aaa0ac8c91bfc11e8ab31779a80
GyuHyeon <rbgusdlza@naver.com>
2024-03-26 00:31:07 +0900

    feat(decimalToBaseN): 파일 생성 및 메서드 구현

---
+++ library/decimalToBaseN.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, b; 
+vector<int> v;
+
+int main() {
+	cin >> n >> b;
+	while(n > 1) {
+		v.push_back(n % b);
+		n /= b;
+	}
+	if(n == 1) v.push_back(1);
+	reverse(v.begin(), v.end());
+	for(int a : v) {
+		// if(a >= 10) 이 조건은 16진법 변환을 위해 필요함.
+		// (16진법 : 0 ~ F로 표현하는 방법)
+		if(a >= 10) cout << char(a + 55);
+		else cout << a;
+	}
+	return 0;
+}
