578a7782b35c2a00eb818aaf4314e64896282ec6
GyuHyeon <rbgusdlza@naver.com>
2024-07-11 23:13:28 +0900

    style(string_add): 들여쓰기 수정

--- library/string_add.cpp
+++ library/string_add.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 string add(string a, string b) {
     int sum = 0;
-	string ret; 
+    string ret; 
     while(a.size() || b.size() || sum){
         if(a.size()) {
             sum += a.back() - '0';
@@ -16,14 +16,14 @@ string add(string a, string b) {
         ret += (sum % 10) + '0'; 
         sum /= 10; 
     } 
-	reverse(ret.begin(), ret.end());
-	return ret;
+    reverse(ret.begin(), ret.end());
+    return ret;
 } 
 
 int main() {
-	string a, b;
-	cin >> a >> b;
+    string a, b;
+    cin >> a >> b;
 	
-	cout << add(a, b) << "\n";
-	return 0;
+    cout << add(a, b) << "\n";
+    return 0;
 }
