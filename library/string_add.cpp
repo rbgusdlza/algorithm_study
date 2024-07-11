895c25adf4411d38a1226407e5ad5fdbc347e49f
GyuHyeon <rbgusdlza@naver.com>
2024-07-11 14:51:10 +0900

    feat(string_add): 파일 생성 및 알고리즘 구현

---
+++ library/string_add.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;   
+
+string add(string a, string b) {
+    int sum = 0;
+	string ret; 
+    while(a.size() || b.size() || sum){
+        if(a.size()) {
+            sum += a.back() - '0';
+            a.pop_back(); 
+        }
+        if(b.size()) {
+            sum += b.back() - '0';
+            b.pop_back(); 
+        }
+        ret += (sum % 10) + '0'; 
+        sum /= 10; 
+    } 
+	reverse(ret.begin(), ret.end());
+	return ret;
+} 
+
+int main() {
+	string a, b;
+	cin >> a >> b;
+	
+	cout << add(a, b) << "\n";
+	return 0;
+}
