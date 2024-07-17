10fd5d0460640d8b2c61eaf0c4d9a000291ba59f
GyuHyeon <rbgusdlza@naver.com>
2024-07-17 18:14:45 +0900

    refactor(decimalToBaseN): 모듈화 작업 완료

--- library/decimalToBaseN.cpp
+++ library/decimalToBaseN.cpp
@@ -3,20 +3,26 @@ using namespace std;
 int n, b; 
 vector<int> v;
 
-int main() {
-	// n: 바꾸려는 수, b: base(기수)
-	cin >> n >> b;
+string decimalToBaseN(int n, int b) {
 	while(n > 1) {
 		v.push_back(n % b);
 		n /= b;
 	}
 	if(n == 1) v.push_back(1);
 	reverse(v.begin(), v.end());
+	string ret = "";
 	for(int a : v) {
 		// 16진법 변환을 위해 필요
 		// (16진법 : 0 ~ F로 표현하는 방법)
-		if(a >= 10) cout << char(a + 55);
-		else cout << a;
+		if(a >= 10) ret += char(a + 55);
+		else ret += a;
 	}
+	return ret;
+}
+
+int main() {
+	// n: 바꾸려는 수, b: base(기수)
+	cin >> n >> b;
+	cout << decimalToBaseN(n, b) << "\n";
 	return 0;
 }
