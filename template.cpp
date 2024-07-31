29e9f511ebe82d7d57adabc4b295869acd43ac8c
GyuHyeon <rbgusdlza@naver.com>
2024-07-31 17:28:32 +0900

    feat(template): 알고리즘용 템플릿 생성

---
+++ template.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastIo cin.tie(0)->sync_with_stdio(0)
+#define INF 987654321
+typedef long long ll;
+
+int main() {
+	fastIo;
+	
+	return 0;
+}
